6bb95661e38d4a304627de277c01dcb4235241b7
Joe <carre.joe3@gmail.com>
2019-12-08 16:02:38 +0000

    added hero image and title into HeroImage component + tests

--- air-quality/src/components/HeroImage.vue
+++ air-quality/src/components/HeroImage.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="heroImageContainer">
-
+    <h1 id="heroImageTitle">{{ heroImageData.title }}</h1>
+    <img id="heroImage" v-bind:src="heroImageData.src" v-bind:alt="heroImageData.title">
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HeroImage'
+  name: 'HeroImage',
+  computed: {
+    heroImageData () {
+      return this.$store.getters.heroImageData
+    }
+  }
 }
 </script>
 
--- air-quality/tests/e2e/specs/test.js
+++ air-quality/tests/e2e/specs/test.js
@@ -1,7 +1,15 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('App starts', () => {
+describe('App starts and elements are present', () => {
   it('Visits the app root url', () => {
     cy.visit('/')
   })
+
+  it("Displays an image in element div with id heroImageContainer", () => {
+    cy.get('div[id="heroImageContainer"]').find("img").should('be.visible');
+  });
+
+  it("H1 element is present within the div with an id of heroImageContainer", () => {
+    cy.get('div[id="heroImageContainer"]').find("h1")
+  });
 })
