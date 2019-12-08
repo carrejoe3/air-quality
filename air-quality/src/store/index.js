2020485e66f362185a0a5ef55cf91d6d8847331a
Joe <carre.joe3@gmail.com>
2019-12-08 15:28:27 +0000

    imported json data into store

--- air-quality/src/store/index.js
+++ air-quality/src/store/index.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import englishData from '../data/english.json'
+// import hindiData from '../data/hindi.json'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    allData: englishData
   },
   mutations: {
   },
