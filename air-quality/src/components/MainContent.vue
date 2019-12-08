a0f3dcaa0729ade017e8765ac5fd450ddda756e5
Joe <carre.joe3@gmail.com>
2019-12-08 16:26:41 +0000

    displayed first five paragraphs within MainContent component + test

--- air-quality/src/components/MainContent.vue
+++ air-quality/src/components/MainContent.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="mainContent">
-
+    <p v-for="content in mainContentData" v-bind:key="content.id">{{content}}</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'MainContent'
+  name: 'MainContent',
+  computed: {
+    mainContentData () {
+      return this.$store.getters.mainContentData
+    }
+  }
 }
 </script>
 
--- air-quality/tests/e2e/specs/test.js
+++ air-quality/tests/e2e/specs/test.js
@@ -12,4 +12,8 @@ describe('App starts and elements are present', () => {
   it("H1 element is present within the div with an id of heroImageContainer", () => {
     cy.get('div[id="heroImageContainer"]').find("h1")
   });
+
+  it("5 <p> tags are present within a div with an id of mainContent", () => {
+    cy.get('div[id="mainContent"]').children('p').should('have.length', 5)
+  });
 })
