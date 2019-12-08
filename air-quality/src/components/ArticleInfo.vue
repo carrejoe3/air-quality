07b7a0bb4922b3e9cceaad74f5cf318003b7f9f4
Joe <carre.joe3@gmail.com>
2019-12-08 18:17:59 +0000

    added article info data into ArticleInfo component

--- air-quality/src/components/ArticleInfo.vue
+++ air-quality/src/components/ArticleInfo.vue
@@ -1,14 +1,30 @@
 <template>
   <div id="articleInfo">
+    <div>{{ articleInfo.byLine }}</div>
+    <div>{{ articleInfo.date }}</div>
+    <div>{{ articleInfo.category }}</div>
+    <a v-bind:href="articleInfo.categoryUrl">{{ articleInfo.categoryUrl }}</a>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'ArticleInfo'
+  name: 'ArticleInfo',
+  computed: {
+    articleInfo () {
+      return this.$store.getters.articleInfo
+    }
+  }
 }
 </script>
 
 <style scoped lang="scss">
-
+#articleInfo {
+  display: flex;
+  justify-content: space-between;
+  text-align: left;
+  max-width: 760px;
+  margin: 0 auto;
+  font-size: 0.7em;
+}
 </style>
