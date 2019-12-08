becc9b06771ac98697217f5d1bdaedfe3fb92a19
Joe <carre.joe3@gmail.com>
2019-12-08 18:51:01 +0000

    added tests for language buttons

--- air-quality/tests/e2e/specs/test.js
+++ air-quality/tests/e2e/specs/test.js
@@ -13,6 +13,10 @@ describe('App starts and elements are present', () => {
     cy.get('div[id="heroImageContainer"]').find("h1")
   });
 
+  it("Two buttons are present within a div with class btnContainer", () => {
+    cy.get('div[class="btnContainer"]').children("button").should('have.length', 2)
+  });
+
   it("A div with an id of articleInfo exists", () => {
     cy.get('div[id="articleInfo')
   });
